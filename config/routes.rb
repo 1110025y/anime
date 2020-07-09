dd6311dfbcc981f553b53055e0ecf9bae3223e68
1110025y <spw57h9@gmail.com>
2020-07-10 04:06:50 +0900

    view_create

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'messages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "messages#index"
+  resources :tweets, only: [:index, :new]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_073418) do
+ActiveRecord::Schema.define(version: 2020_07_09_075524) do
+
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "text"
+    t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
